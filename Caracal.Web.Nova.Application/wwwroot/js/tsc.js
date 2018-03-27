b8c1268994b0519f1e15ee81c358ceb835db3352
Ettiene Mare <ettienemare@gmail.com>
2018-03-27 18:50:30 +0200

    Worflow designer complete

---
+++ Caracal.Web.Nova.Application/wwwroot/js/tsc.js
@@ -0,0 +1,14 @@
+define("test", ["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var Test = /** @class */ (function () {
+        function Test() {
+        }
+        Test.prototype.testMethod = function () {
+            alert("ABCDEFGHIJK");
+        };
+        return Test;
+    }());
+    exports.Test = Test;
+});
+//# sourceMappingURL=tsc.js.map
