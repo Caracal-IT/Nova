20781cb4bf6f24209e7a416739991c880f5ba8f4
Ettiene Mare <ettienemare@gmail.com>
2018-04-04 08:00:18 +0200

    Worflow designer complete

--- Caracal.Web.Nova.Builder/ClientApp/app/components/home/home.component.ts
+++ Caracal.Web.Nova.Builder/ClientApp/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'home',
+    styleUrls: ['home.component.html'],
     templateUrl: './home.component.html'
 })
 export class HomeComponent {
